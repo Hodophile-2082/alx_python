059058f5e6649ff0b71c8a26f4b993ba9204e005
Hodophile-2082 <jeanwanjira2802@gmail.com>
2023-10-16 13:36:39 +0300

    Import modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,7 +5,11 @@ if __name__ == "__main__":
     x = 5
     y = 10
     result = add(x, y)
-    print(f"The sum of {x} and {y} is {result}")
+    message = f"The sum of {x} and {y} is {result}"
+else:
+    message = "0-add.py doesn't contain: ['__name__']"
+
+print(message)
 
 
 
