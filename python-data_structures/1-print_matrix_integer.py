5ce35ec5eb14a5300f04665e4b343e824afb6d51
Hodophile-2082 <jeanwanjira2802@gmail.com>
2023-10-22 15:08:57 +0300

    Python lists

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,14 @@
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
+    max_width = max(len(str(num)) for row in matrix for num in row)
+
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
-            print()
+        for num in row:
+            if num < 0:
+                print("{:d}".format(num), end=" " * (max_width - len(str(num)) + 1))
+            else:
+                print(" " * (max_width - len(str(num)) + 1), end="")
+                print("{:d}".format(num), end=" ")
+        print()
