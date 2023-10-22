3001e4d3291c1a2305755b009842b02f92e070f2
Hodophile-2082 <jeanwanjira2802@gmail.com>
2023-10-22 14:16:08 +0300

    raising exceptions

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,4 +2,4 @@ def raise_exception():
     try:
         raise TypeError("This is a custom TypeError exception")
     except TypeError as e:
-        print("Exception raised")
+        print("Exception has been raised")
