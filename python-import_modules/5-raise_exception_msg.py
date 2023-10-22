1f68ecded8d6768576bc591e6228fae371d5481a
Hodophile-2082 <jeanwanjira2802@gmail.com>
2023-10-22 14:22:27 +0300

    raising exceptions with custom messages

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,18 +1,6 @@
-class NameException(Exception):
-    pass
-
-def raise_exception_msg(message=""):
+def raise_exception_msg(message="C is fun"):
     if message:
         raise NameException(message)
     else:
         raise NameException("A NameException occurred")
 
-try:
-    raise_exception_msg("This is a custom NameException with a message")
-except NameException as e:
-    print(f"Caught a NameException: {e}")
-
-try:
-    raise_exception_msg()
-except NameException as e:
-    print(f"Caught a NameException: {e}")
